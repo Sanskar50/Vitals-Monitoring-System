1264c1c27637702eac1dd588ffd7e7991994557d
Sanskar Suri <sanskarsuri50@gmail.com>
2024-03-15 01:55:33 +0530

    added 2 views & pagetitles in routes

--- README.md
+++
@@ -1,2 +0,0 @@
-# Vitals-Monitoring-System
- A web app to monitor patients vitals in real time
--- index.js
+++ index.js
@@ -8,8 +8,30 @@ app.set('views', 'views');
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/',(req,res,next)=>{
-    res.render('tabs/vitals');
+app.use('/vitals',(req,res,next)=>{
+    res.render('tabs/vitals',{
+        pageTitle:'Vitals',
+    });
 })
+
+app.use('/ml-model',(req,res,next)=>{
+    res.render('tabs/ml-model',{
+        pageTitle:'ML-Model',
+    });
+})
+
+app.use('/about',(req,res,next)=>{
+    res.render('tabs/about',{
+        pageTitle:'About',
+    });
+})
+
+app.use('/',(req,res,next)=>{
+    res.render('tabs/vitals',{
+        pageTitle:'Home Page',
+    });
+})
+
+
 
 app.listen(3000);
